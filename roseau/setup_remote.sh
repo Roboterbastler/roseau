d19e37f1359fcb1ba85cf1e8fd4520620cb0a97e
Jacob Seibert <roboterbastler@posteo.de>
2017-05-07 16:44:04 +0200

    setup remote

--- roseau/setup_remote.sh
+++ roseau/setup_remote.sh
@@ -1,8 +1,8 @@
 ROSEAU_HOST=roseau
 
-ROSEAU_IP=$(getent hosts $ROSEAU_HOST | awk '{ print $1 }')
+ROSEAU_IP=$(getent hosts $ROSEAU_HOST | awk '{ print $1 ; exit }')
 
-export ROS_MASTER_URI=http://$ROSEAU_HOST:11311
+export ROS_MASTER_URI=http://$ROSEAU_IP:11311
 export ROS_IP=$ROSEAU_IP
 
 printf "Configured environment variables for connecting to %s (%s).\n" $ROSEAU_HOST $ROSEAU_IP
