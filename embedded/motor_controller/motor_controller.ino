71517566598c21107c213b7638f3c7b7b7c78bd4
Jacob Nikolai Emanuel Seibert <j.seibert@campus.tu-berlin.de>
2017-07-18 19:25:52 +0200

    Add motor controller arduino sketch and arduino libraries

---
+++ embedded/motor_controller/motor_controller.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+
+// The ESC motor controller
+Servo motorController;
+
+// The steering servo
+Servo steering;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
